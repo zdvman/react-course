209b3c1eda3331c221b785938d9fe879e3bc5daf
zdvman <zuyevdmytro@gmail.com>
2025-09-05 19:26:57 +0200

    add 10 React-quiz app

--- 10-react-quiz/react-quiz/src/components/App.js
+++ 10-react-quiz/react-quiz/src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
   status: 'loading',
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -23,14 +24,22 @@ function reducer(state, action) {
     case 'start':
       return { ...state, status: 'active' };
     case 'newAnswer':
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error('Action is unknown');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -52,7 +61,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- 10-react-quiz/react-quiz/src/components/Options.js
+++ 10-react-quiz/react-quiz/src/components/Options.js
@@ -1,8 +1,20 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const isAnswer = answer !== null;
   return (
     <div className='options'>
-      {question.options.map((option) => (
-        <button key={option} className='btn btn-option'>
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            isAnswer
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+          disabled={isAnswer}
+        >
           {option}
         </button>
       ))}
