1d0a754a02c11ff28eee63705cfcbca9be5717b1
zdvman <zuyevdmytro@gmail.com>
2025-09-18 17:49:42 +0200

    add leafler react to the app

--- 11-worldwise/worldwise/src/components/Map.jsx
+++ 11-worldwise/worldwise/src/components/Map.jsx
@@ -1,25 +1,44 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
 
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
+      // onClick={() => {
+      //   navigate('form');
+      // }}
     >
-      <h1>Map</h1>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      {/* <h1>Map</h1>
       <h2>
         Position {lat} {lng}
       </h2>
       <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
         Change Position
-      </button>
+      </button> */}
     </div>
   );
 }
